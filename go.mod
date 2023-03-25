049613760620cf05ce1d8940695d0cdfce44f360
Digisata <hnaufal123@gmail.com>
2023-03-25 14:52:44 +0700

    added auth middleware

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/gofiber/fiber/v2 v2.42.0
+	github.com/golang-jwt/jwt/v5 v5.0.0-rc.1
 	gorm.io/driver/postgres v1.4.8
 	gorm.io/gorm v1.24.6
 )
--- main.go
+++ main.go
@@ -5,16 +5,28 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/golang-jwt/jwt/v5"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+type User struct {
+	gorm.Model
+	FullName string `json:"full_name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 type Item struct {
 	gorm.Model
-	Name string `json:"name"`
-	Qty  int    `json:"qty"`
+	Name   string `json:"name"`
+	Qty    int    `json:"qty"`
+	UserID int    `json:"user_id"`
+	User   User   `json:"user"`
 }
 
 var DB *gorm.DB
@@ -29,15 +41,131 @@ func init() {
 }
 
 func main() {
-	DB.AutoMigrate(&Item{})
+	DB.AutoMigrate(&User{}, &Item{})
 
 	app := fiber.New()
+
+	app.Use(cors.New())
+
+	authMiddleware := func(c *fiber.Ctx) error {
+		tokenHeaders := c.Get("Authorization")
+		if tokenHeaders == "" || !strings.HasPrefix(tokenHeaders, "Bearer ") {
+			return c.Status(http.StatusUnauthorized).JSON(fiber.Map{
+				"success": false,
+				"message": "",
+				"data":    nil,
+			})
+		}
+
+		token, err := jwt.Parse(strings.Split(tokenHeaders, " ")[1], func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+
+			return []byte("JWT_SECRET"), nil
+		})
+		if err != nil {
+			return c.Status(http.StatusUnauthorized).JSON(fiber.Map{
+				"success": false,
+				"message": "",
+				"data":    nil,
+			})
+		}
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			c.Locals("user_id", claims["user_id"])
+		} else {
+			return c.Status(http.StatusUnauthorized).JSON(fiber.Map{
+				"success": false,
+				"message": "",
+				"data":    nil,
+			})
+		}
+
+		return c.Next()
+	}
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello world 2!")
 	})
 
-	app.Post("/items", func(c *fiber.Ctx) error {
+	app.Post("/register", func(c *fiber.Ctx) error {
+		user := User{}
+		err := c.BodyParser(&user)
+		if err != nil {
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+				"success": false,
+				"message": err.Error(),
+				"data":    nil,
+			})
+		}
+
+		err = DB.Create(&user).Error
+		if err != nil {
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+				"success": false,
+				"message": err.Error(),
+				"data":    nil,
+			})
+		}
+
+		return c.JSON(fiber.Map{
+			"success": true,
+			"message": "",
+			"data":    user,
+		})
+	})
+
+	app.Post("/login", func(c *fiber.Ctx) error {
+		userRequest := User{}
+		err := c.BodyParser(&userRequest)
+		if err != nil {
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+				"success": false,
+				"message": err.Error(),
+				"data":    nil,
+			})
+		}
+
+		user := User{}
+		result := DB.Where("email = ?", userRequest.Email).First(&user)
+		if result.RowsAffected == 0 {
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+				"success": false,
+				"message": "Email or password invalid",
+				"data":    nil,
+			})
+		}
+
+		if userRequest.Password != user.Password {
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+				"success": false,
+				"message": "Email or password invalid",
+				"data":    nil,
+			})
+		}
+
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"user_id": user.ID,
+		})
+
+		tokenString, err := token.SignedString([]byte("JWT_SECRET"))
+		if err != nil {
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+				"success": false,
+				"message": err.Error(),
+				"data":    nil,
+			})
+		}
+
+		return c.JSON(fiber.Map{
+			"success": true,
+			"message": "",
+			"data":    tokenString,
+		})
+	})
+
+	app.Post("/items", authMiddleware, func(c *fiber.Ctx) error {
 		item := Item{}
 		err := c.BodyParser(&item)
 		if err != nil {
@@ -48,7 +176,16 @@ func main() {
 			})
 		}
 
-		DB.Create(&item)
+		item.UserID = int(c.Locals("user_id").(float64))
+
+		err = DB.Create(&item).Error
+		if err != nil {
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+				"success": false,
+				"message": err.Error(),
+				"data":    nil,
+			})
+		}
 
 		return c.JSON(fiber.Map{
 			"success": true,
@@ -57,7 +194,7 @@ func main() {
 		})
 	})
 
-	app.Get("/items", func(c *fiber.Ctx) error {
+	app.Get("/items", authMiddleware, func(c *fiber.Ctx) error {
 		name := c.Query("name")
 		items := []Item{}
 
@@ -78,7 +215,14 @@ func main() {
 			})
 		}
 
-		DB.Find(&items)
+		err := DB.Preload("User").Where("user_id = ?", c.Locals("user_id")).Find(&items).Error
+		if err != nil {
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+				"success": false,
+				"message": err.Error(),
+				"data":    nil,
+			})
+		}
 
 		return c.JSON(fiber.Map{
 			"success": true,
@@ -87,7 +231,7 @@ func main() {
 		})
 	})
 
-	app.Get("/items/:id", func(c *fiber.Ctx) error {
+	app.Get("/items/:id", authMiddleware, func(c *fiber.Ctx) error {
 		item := Item{}
 		id, err := strconv.Atoi(c.Params("id"))
 		if err != nil {
@@ -98,7 +242,7 @@ func main() {
 			})
 		}
 
-		result := DB.First(&item, id)
+		result := DB.Preload("User").Where("user_id = ?", c.Locals("user_id")).First(&item, id)
 		if result.RowsAffected != 0 {
 			return c.JSON(fiber.Map{
 				"success": true,
@@ -114,7 +258,7 @@ func main() {
 		})
 	})
 
-	app.Put("/items/:id", func(c *fiber.Ctx) error {
+	app.Put("/items/:id", authMiddleware, func(c *fiber.Ctx) error {
 		id, err := strconv.Atoi(c.Params("id"))
 		if err != nil {
 			return c.Status(http.StatusBadRequest).JSON(fiber.Map{
@@ -124,8 +268,8 @@ func main() {
 			})
 		}
 
-		item := Item{}
-		err = c.BodyParser(&item)
+		itemRequest := Item{}
+		err = c.BodyParser(&itemRequest)
 		if err != nil {
 			return c.Status(http.StatusBadRequest).JSON(fiber.Map{
 				"success": false,
@@ -134,9 +278,7 @@ func main() {
 			})
 		}
 
-		item.ID = uint(id)
-
-		if item.Name == "" {
+		if itemRequest.Name == "" {
 			return c.Status(http.StatusBadRequest).JSON(fiber.Map{
 				"success": false,
 				"message": "Name is required",
@@ -144,12 +286,25 @@ func main() {
 			})
 		}
 
-		result := DB.Save(&item)
+		item := Item{}
+		result := DB.Preload("User").Where("user_id = ?", c.Locals("user_id")).First(&item, id)
+		if result.RowsAffected == 0 {
+			return c.Status(http.StatusNotFound).JSON(fiber.Map{
+				"success": false,
+				"message": fmt.Sprintf("Item with ID %d not found", id),
+				"data":    nil,
+			})
+		}
+
+		itemRequest.ID = item.ID
+		itemRequest.UserID = item.UserID
+
+		result = DB.Save(&itemRequest)
 		if result.RowsAffected != 0 {
 			return c.JSON(fiber.Map{
 				"success": true,
 				"message": "",
-				"data":    item,
+				"data":    itemRequest,
 			})
 		}
 
@@ -160,7 +315,7 @@ func main() {
 		})
 	})
 
-	app.Delete("/items/:id", func(c *fiber.Ctx) error {
+	app.Delete("/items/:id", authMiddleware, func(c *fiber.Ctx) error {
 		id, err := strconv.Atoi(c.Params("id"))
 		if err != nil {
 			return c.Status(http.StatusBadRequest).JSON(fiber.Map{
@@ -170,7 +325,7 @@ func main() {
 			})
 		}
 
-		result := DB.Delete(&Item{}, id)
+		result := DB.Where("user_id = ?", c.Locals("user_id")).Delete(&Item{}, id)
 		if result.RowsAffected != 0 {
 			return c.JSON(fiber.Map{
 				"success": true,
